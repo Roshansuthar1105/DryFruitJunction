04cd5f1f30ea87e053ebf013d6d0549e3e7f07b8
roshansuthar1105 <roshansuthar2023@gmail.com>
2025-07-31 20:31:13 +0530

    extra logs removed

--- client/src/pages/ProductsPage.jsx
+++ client/src/pages/ProductsPage.jsx
@@ -24,8 +24,6 @@ export default function ProductsPage() {
     const fetchProducts = async () => {
       try {
         const response = await axios.get(`${BACKEND_API}/api/products`);
-        
-        console.table(response.data.data);
         setProducts(response.data.data);
       } catch (error) {
         console.error('Failed to fetch products:', error);
